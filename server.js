dfdcb041c27af745f0debedc61a080ae9c660ffa
VIbitoye <112020449+VIbitoye@users.noreply.github.com>
2023-04-07 17:20:58 -0400

    new

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectDB = require('./src/Pages/Authentication/db.js');
+const app = express();
+
+connectDB();
+
+app.get('/', (req,res) => res.send('API Running'));
+
+app.use(express.json());
+app.use('/src/Pages/Authentication/Employee_Model' , require('./src/Pages/Authentication/Employee_Model'));
+
+app.use(express.json({extended: false}));
+// "start": "react-scripts start",
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> console.log('Server started on port', PORT));
+
